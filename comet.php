75ee85f687a4227fcfa043189f7513440a07fe43
yasuto777 <basilisk.0331@icloud.com>
2016-04-27 23:36:13 +0900

    Initial commit

---
+++ comet.php
@@ -0,0 +1,44 @@
+<?php
+//MySQL setting
+include('./localdb.php');
+
+$mysqli = mysqli_connect($host,$dbuser,$dbpass,$db);
+
+//Check connectin
+if(mysqli_connect_errno()){
+	printf("Connect failed: %s\n",mysqli_connect_errno());
+	exit();
+}
+
+// First load
+function load(){
+	if($result = mysqli_query($mysqli,"SELECT * FROM $dbtable")){
+		//Get associative array
+		while($row = mysqli_fetch_assoc($result)){
+			echo "<div class=\"arrow_name\">".htmlspecialchars($row['user'])."</div>";
+			echo "<div class=\"arrow_box\">".htmlspecialchars($row['message'])."</div>";
+			echo "<div class=\"send_time\">".$row['dt']."</div>";
+			$id = $row['id'];
+		}
+		//Free $result
+		mysqli_free_result($result);
+	}
+	//Close connection
+	mysqli_close($mysqli);
+}
+
+//Check Updata & reload
+function update(){
+	
+}
+
+switch($_POST['mode']){
+case 0://First load
+	load();
+	break;
+case 1://check update
+	update();
+	break;
+}
+
+?>
