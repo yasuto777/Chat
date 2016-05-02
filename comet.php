c1b2f165b0110f14ac50c1e2f34bb0bca61fb25a
yasuto777 <basilisk.0331@icloud.com>
2016-05-03 00:15:34 +0900

    Redesigned comet.php

--- comet.php
+++ comet.php
@@ -1,26 +1,53 @@
 <?php
 
-// First load
-function load(){
-
-include('./heroku_MySQL.php');
-
+// Include MySQL setting
+include './localdb.php';
 $mysqli = mysqli_connect($host,$dbuser,$dbpass,$db);
 
-//Check connectin
-if(mysqli_connect_errno()){
-	printf("Connect failed: %s\n",mysqli_connect_errno());
-	exit();
+// Select mode
+$mode = $_POST['mode'];
+
+// $tmp <- newer, $id <- older
+//$id;
+
+
+switch($mode){
+	//First load
+case 0:
+	load();
+	break;
+	//Send
+case 1:
+	send();
+	break;
+	//comet
+case 2:
+	comet();
+	break;
 }
 
-	if($result = mysqli_query($mysqli,"SELECT * FROM $dbtable")){
+//{{{ function load
+function load(){
+	//Setting for use database variables
+	global $host;
+	global $dbuser;
+	global $dbpass;
+	global $db;
+	global $dbtable;
+	global $mysqli;
 
+	//Check connection
+	if(mysqli_connect_errno()){
+		printf("Connect failed: %s\n",mysqli_connect_errno());
+		exit();
+	}
+
+	if($result = mysqli_query($mysqli,"SELECT * FROM $dbtable")){
 		//Get associative array
 		while($row = mysqli_fetch_assoc($result)){
 			echo "<div class=\"arrow_name\">".htmlspecialchars($row['user'])."</div>";
 			echo "<div class=\"arrow_box\">".htmlspecialchars($row['message'])."</div>";
 			echo "<div class=\"send_time\">".$row['dt']."</div>";
-			$id = $row['id'];
 		}
 		//Free $result
 		mysqli_free_result($result);
@@ -28,20 +55,27 @@ if(mysqli_connect_errno()){
 	//Close connection
 	mysqli_close($mysqli);
 }
+//}}}
 
-//Check Updata & reload
-function update(){
-	
+//{{{ function send
+function send(){
+	$user = htmlspecialchars($_POST['user']);
+	$message = htmlspecialchars($_POST['message']);
+	$now = date("Y-m-d H:i:s");
+
+	//Send query to MySQL
+	$result = mysqli_query($mysqli,"INSERT INTO $dbtable (user,message,dt) VALUES('$user','$message','$now')");
+
+	if(!$result){
+		$err_message = 'Invalid query: '.mysqli_errno()."\n";
+		$err_message .= 'Whole query';
+		die($err_message);
+	}
 }
+//}}}
 
-$mode = $_POST['mode'];
-switch($mode){
-case 0://First load
-	load();
-	break;
-case 1://check update
-	update();
-	break;
+//{{{ function comet
+function comet(){
 }
-
+//}}}
 ?>
