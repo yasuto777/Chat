bca2f9a5e19e797cd7f66c886c5e91c3468734f5
yasuto777 <basilisk.0331@icloud.com>
2016-05-03 00:17:02 +0900

    Add a description of the mode

--- js/commet.js
+++ js/commet.js
@@ -1,13 +1,25 @@
-function comet(mode){
-	$.ajax({
-		type: 'POST',
-		url: "./comet.php",
-		dataType: 'html',
-		data: {
-			mode: mode
-		},
-		success: function(data){
-			$('.outputField').append(data);
-		}
-	});
-}
+// mode 0: load
+// mode 1: send
+// mode 2: comet
+
+// func load
+//function load(){
+//	$.ajax({
+//		type: 'POST',
+//		url: "./comet.php",
+//		dataType: 'html',
+//		data: {mode: 0},
+//		success: function(data){
+//			$('.outputField').append(data);
+//			}
+//	});
+//}
+
+$.ajax({
+	type:'POST',
+	url: "./comet.php",
+	data:{id: $id},
+	success: function(data){
+		$('.outputField').append(data);
+	}
+});
